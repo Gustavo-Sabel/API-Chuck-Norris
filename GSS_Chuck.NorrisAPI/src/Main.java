cc3b7dee1c8f117aabbc096bf364f782ea576865
GUSTAVO DE SOUZA SABEL <gustavo_sabel@estudante.sesisenai.org.br>
2024-02-21 21:43:01 -0300

    Ajustando o metodo main

--- GSS_Chuck.NorrisAPI/src/Main.java
+++ GSS_Chuck.NorrisAPI/src/Main.java
@@ -7,29 +7,28 @@ public class Main {
     public static void main(String[] args) {
         try {
             String piada = obterPiadaChuckNorris();
+            // URL da API do Chuck Norris
+            String apiUrl = "https://api.chucknorris.io/jokes/random";
+
+            //Fazendo a requisição GET para a API
+            HttpURLConnection conexao = (HttpURLConnection) new URL(apiUrl).openConnection();
+            conexao.setRequestMethod("GET");
+
+            //Lendo a resposta do API
+            BufferedReader leitor = new BufferedReader(
+                    new InputStreamReader(conexao.getInputStream()));
+            StringBuilder resposta = new StringBuilder();
+            String linha;
+            while((linha = leitor.readLine()) != null){
+                resposta.append(linha);
+            }
+            leitor.close(); // Fecha a entrada de memoria
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     private static String obterPiadaChuckNorris() throws IOException {
-        // URL da API do Chuck Norris
-        String apiUrl = "https://api.chucknorris.io/jokes/random";
-
-        //Fazendo a requisição GET para a API
-        HttpURLConnection conexao = (HttpURLConnection) new URL(apiUrl).openConnection();
-        conexao.setRequestMethod("GET");
-
-        //Lendo a resposta do API
-        BufferedReader leitor = new BufferedReader(
-                new InputStreamReader(conexao.getInputStream()));
-        StringBuilder resposta = new StringBuilder();
-        String linha;
-        while((linha = leitor.readLine()) != null){
-            resposta.append(linha);
-        }
-        leitor.close(); // Fecha a entrada de memoria
-
         //Extraindo a piada do JSON
         int iniciodoindice = resposta.indexOf("\"value\":") + ("\"value\":".length());
         int fimdoindice = resposta.lastIndexOf("\"");
